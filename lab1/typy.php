d51ab74f77c1edbe697c4a096fc34d73c89c9e7f
Mat <106345025+nocush@users.noreply.github.com>
2022-10-12 13:43:24 +0200

    create typy.php

---
+++ lab1/typy.php
@@ -0,0 +1,30 @@
+<!DOCTYPE hmtl>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			<?php
+				echo "typy"
+			?>
+		</title>
+	</head>
+	<body>
+		<?php
+            $a=1234;
+            $b=567.789;
+            $c=1;
+            $d=0;
+            $e=true;
+            $f="0";
+            $g="Typy w PHP";
+            $h=[1,2,3,4];
+            $i=[];
+            $j=["zielony", "czerwony", "niebieski"];
+            $k=["Agata", "Agatowska", 4.67, true];
+            $time = gettimeofday();
+            echo $time;
+
+		?>
+	</body>
+</html>
